13f3d40b68087c4008b3b009bd9c0ce516aec576
Alexander Kuznetsov <Alexander Kuznetsov>
2016-11-24 02:00:04 -0500

    A* Algorithm returning boolean array of shortest path

--- MazeAndAlgorithm/src/Vector.cpp
+++
@@ -1,8 +0,0 @@
-#include "Vector.h"
-#include <math.h>
-
-float Vector::length(){
-	return sqrt((x*x) + (y*y) + (z*z));
-}
-Vector::Vector() : x(0), y(0), z(0) {}
-Vector::Vector(int x, int y, int z) : x(x), y(y), z(z) {}
